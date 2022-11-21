cb51b2e11f8f16c7cc24740093388561a4a3a325
unknown <tommasocaputi85@gmail.com>
2022-11-21 12:06:57 +0100

    update run.py

--- app/run.py
+++ app/run.py
@@ -1,3 +1,7 @@
 #this file should become an exe which run the background app 
-import os
-os.startfile("C:\\Users\\CLASSE 5^CI\\Desktop\\fast-ftp\\app\\main.py")
+import subprocess
+
+info = subprocess.STARTUPINFO()
+info.dwFlags=1
+info.wShowWindow=0 
+subprocess.Popen(['C:\\Users\\CLASSE 5^CI\\Desktop\\fast-ftp\\app\\main.py'], shell=True, creationflags=subprocess.SW_HIDE)
