3c0185969890bcbf0326087a752552244d66da98
unknown <tommasocaputi85@gmail.com>
2022-11-21 09:17:11 +0100

    prova

--- app/main.py
+++ app/main.py
@@ -8,6 +8,8 @@ FTP_USERNAME = os.getenv('FTP_USERNAME')
 FTP_PASSWORD = os.getenv('FTP_PASSWORD')
 FTP_HOST = os.getenv('FTP_HOST')
 
+print("start")
+
 ftp = FTP()
 ftp.connect(FTP_HOST, 21)
 ftp.login(FTP_USERNAME, FTP_PASSWORD)
