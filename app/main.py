fe82532aed7ed991b0220fdd585c144fa6b4dbeb
unknown <tommasocaputi85@gmail.com>
2022-11-21 11:09:05 +0100

    update fast save function

--- app/main.py
+++ app/main.py
@@ -7,16 +7,24 @@ load_dotenv()
 FTP_USERNAME = os.getenv('FTP_USERNAME')
 FTP_PASSWORD = os.getenv('FTP_PASSWORD')
 FTP_HOST = os.getenv('FTP_HOST')
+FTP_DIRECTORY = os.getenv('FTP_DIRECTORY')
+FILES = os.getenv('FILES')
 
-print("start")
-
-ftp = FTP()
-ftp.connect(FTP_HOST, 21)
+ftp = FTP(FTP_HOST, encoding="latin-1")
 ftp.login(FTP_USERNAME, FTP_PASSWORD)
-print(ftp.getwelcome())
+ftp.cwd(FTP_DIRECTORY)
+
+def fast_save():
+    for f in FILES.split(","):
+        split = f.split("\\")
+        filename = split[len(split)-1]
+        file = open(f,"rb")
+        ftp.storbinary("STOR "+filename, file)
+        file.close()
 
 def save_on_ftp():
     print("ctrl+s pressed")
+    fast_save()
 
 def on_press(key):
     try:
